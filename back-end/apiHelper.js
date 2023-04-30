8e549eae1c507039544f66f2f4d067b73dfb0bed
AnthonyGrod <tantien02@hotmail.com>
2023-04-30 19:09:44 +0200

    Added error handling

--- back-end/apiHelper.js
+++ back-end/apiHelper.js
@@ -1,24 +1,7 @@
-function getCountryData(countryName, request, countryUrl) {
+function getData(name, request, url) {
 	return new Promise((resolve, reject) => {
 		request({
-			url: `${countryUrl}${countryName}`,
-			json: true,
-		}, (err, response, body) => {
-			if (err)
-				reject(err);
-			else if (response.statusCode !== 200)
-				reject(`An error occured. Response status code: ${response.statusCode}`);
-			else
-				resolve(body);
-		});
-	});
-}
-
-
-function getContinentData(continent, request, continentUrl) {
-	return new Promise((resolve, reject) => {
-		request({
-			url: `${continentUrl}${continent}`,
+			url: `${url}${name}`,
 			json: true,
 		}, (err, response, body) => {
 			if (err)
@@ -33,15 +16,16 @@ function getContinentData(continent, request, continentUrl) {
 
 async function getCountryList(continent, number, continentUrl) {
 	const request = require("request");
-	let continentData = await getContinentData(continent, request, continentUrl);
-	let countries = continentData.map((country) => country.name.common);
+	let continentData = await getData(continent, request, continentUrl);
+	
+	const countries = continentData.map((country) => country.name.common);
 	return countries.slice(0, number);
 }
 
 
 async function getCountryInfo(countryName, countryUrl) {
 	const request = require("request");
-	let countryData = await getCountryData(countryName, request, countryUrl);
+	let countryData = await getData(countryName, request, countryUrl);
 	let countryInfo = {
 		name: countryData[0].name.official,
 		capital: countryData[0].capital,
--- back-end/server.js
+++ back-end/server.js
@@ -7,8 +7,13 @@ const PORT = 8080;
 const COUNTRY_URL = 'https://restcountries.com/v3.1/name/';
 const CONTINENT_URL = 'https://restcountries.com/v3.1/region/';
 
-function selector(min, max) {
+function selector(min, max, redirected) {
+	let body = ``;
+	if (redirected) {
+		body = `<p>Chosen continent does not have that many countries available.</p>`;
+	}
 	return `<html>
+				${body}
 				<body>
 					<form method="post" action="/show">
 						<label for="continent">Choose your continent:</label>
@@ -21,6 +26,7 @@ function selector(min, max) {
 							<option value="Africa">Africa</option>
 							<option value="Antarctica">Antarctica</option>
 						</select>
+						</br>
 						<label for="number">Choose a number between 2 and 10:</label>
 							<input type="number" id="number" name="number" min=${min} max=${max} required>
 						<button type="submit">Submit</button>
@@ -47,41 +53,51 @@ function show(req, res) {
 		const data = qs.parse(body);
 		const continent = data.continent;
 		const number = parseInt(data.number);
-		const countryList = await getCountryList(continent, number, CONTINENT_URL);
-		countryList.sort(function (a, b) {
-			if (a > b)
-				return 1;
-			if (a < b)
-				return -1;
-			return 0;
-		});
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write(`\r\n<html><body>`);
-		for (const country of countryList) {
-			let countryInfo = await getCountryInfo(country, COUNTRY_URL);
-			let languagesList = Object.values(countryInfo.languages);
-			const firstCurrencyCode = Object.keys(countryInfo.currencies)[0];
-			const firstCurrencyName = countryInfo.currencies[firstCurrencyCode].name;
-			res.write(`
-				<p>
-				${checkIfDefined("Country Name", countryInfo.name)}
-				${checkIfDefined("Capital", countryInfo.capital)}
-				${checkIfDefined("Population", countryInfo.population)}
-				${checkIfDefined("Currency", firstCurrencyName)}
-				${checkIfDefined("Subregion", countryInfo.subregion)}
-				${checkIfDefined("Languages", languagesList)}
-				</p>
-				`);
+		try {
+			const countryList = await getCountryList(continent, number, CONTINENT_URL);
+			if (countryList.length < number) {
+			  throw new Error();
+			}
+			countryList.sort(function (a, b) {
+				if (a > b)
+					return 1;
+				if (a < b)
+					return -1;
+				return 0;
+			});
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(`\r\n<html><body>`);
+			for (const country of countryList) {
+				let countryInfo = await getCountryInfo(country, COUNTRY_URL);
+				let languagesList = Object.values(countryInfo.languages);
+				const firstCurrencyCode = Object.keys(countryInfo.currencies)[0];
+				const firstCurrencyName = countryInfo.currencies[firstCurrencyCode].name;
+				res.write(`
+					<p>
+					${checkIfDefined("Country Name", countryInfo.name)}
+					${checkIfDefined("Capital", countryInfo.capital)}
+					${checkIfDefined("Population", countryInfo.population)}
+					${checkIfDefined("Currency", firstCurrencyName)}
+					${checkIfDefined("Subregion", countryInfo.subregion)}
+					${checkIfDefined("Languages", languagesList)}
+					</p>
+					`);
+			}
+			res.write(`</body></html>\r\n`);
+			res.end();
+		} catch (error) {
+			console.error(error);
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(generateResponse('/', 2, 10, true));
+			res.end();
 		}
-		res.write(`</body></html>\r\n`);
-		res.end();
 	});
 }
 
 
-function generateResponse(url, min, max) {
+function generateResponse(url, min, max, redirected) {
 	if (url === '/')
-		return selector(min, max);
+		return selector(min, max, redirected);
 	else
 		return `<html><body><p>404 Not Found</p></body></html>`;
 }
@@ -89,6 +105,7 @@ function generateResponse(url, min, max) {
 
 const server = http.createServer((req, res) => {
 	const url = req.url;
+	var statusCode = res.statusCode;
 	if (url === '/') {
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write(generateResponse(url, 2, 10));
